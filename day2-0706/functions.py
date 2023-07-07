f04d9c4dfd5d3f6b54dcf1b79a2febeea8f745a2
babyshout <lsms0523@gmail.com>
2023-07-06 21:58:55 -0700

    What I learned in 'py class' functions, for loop, conditional statements(if, elif ~~ stuff in the ()) and How get user input in py
    def function_name(maybe args..) :
        statements1 .. x
    in py, block is indets(tap)
    
    for i in range(1, 10, 2) :
        statements
        print(i)
    in range function, If you want use stepside, you should put the 3 parameter
    
    conditional statements if, elif, else	ok
    
    user input
    var_name1 = input('anything what you wanna print')
    print(var_name1)
    
    If you put the parameter to the input function, It will print to the cmd line

---
+++ day2-0706/functions.py
@@ -0,0 +1,75 @@
+functions.py
+# Phillip Forkner
+# July 6, 2023
+# This program writes the word
+# MISSISSIPPI
+
+# import the math package
+import math
+
+print(math.sqrt(2))
+print(math.sin(1))
+
+# import the random package
+import random
+
+# print a pseudo-random (fake) number between 0 and 1
+# 0 is included and 1 is excluded
+# i.e. 0.00000000 -- 0.9999999999
+print(random.random())
+
+
+# define a function called printS()
+# use indentation 
+def printS() :
+	print(' SSSS')
+	print('SS  ')
+	print('  SSS')
+	print('SSSS')
+
+def printI() :
+	print('IIIII')
+	print('  I')
+	print('  I')
+	print('IIIII')
+
+print('M   M')
+print('M M M')
+print('M   M')
+print('M   M')
+
+print()
+printI()
+
+print('')
+printS();
+
+print('')
+printS()
+
+print()
+printI()
+
+print('')
+printS()
+
+print('')
+printS()
+
+print()
+printI()
+
+print()
+print('PPPP ')
+print('P   P')
+print('PPP ')
+print('P')
+
+print()
+print('PPPP ')
+print('P   P')
+print('PPP ')
+print('P')
+
+print()
+printI()
---
+++ day2-0706/loops.py
@@ -0,0 +1,8 @@
+loops.py
+# for loop
+# i is a variable (identifier)
+# range(n) returns a list of numbers from 0 to n
+# 0 is included, n is not
+
+for i in range(0, 6, 2) :
+	print(i)
