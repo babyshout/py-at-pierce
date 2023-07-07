977a45d97635a6d5f88058df61bcd7abaf3235e6
babyshout <lsms0523@gmail.com>
2023-07-06 22:05:18 -0700

    I missed add some files. It is subordinated '0706's commit'

---
+++ day2-0706/ifStatements.py
@@ -0,0 +1,7 @@
+ifStatements.py
+number = 6
+if (number%2 != 0) :
+	# this only happens if the things in () is true
+	print(number, 'is odd')
+else :
+	print(number, 'is even')
---
+++ day2-0706/userInput.py
@@ -0,0 +1,3 @@
+userInput.py
+name = input('What is your name? ')
+print('Hello, ', name)
