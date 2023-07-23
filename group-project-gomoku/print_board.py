988d2986e1bca4c98664f2dfac2a8b32e86f6209
babyshout <lsms0523@gmail.com>
2023-07-23 22:16:15 +0900

    0723 py commit some adjustment of print_board

--- group-project-gomoku/print_board.py
+++ group-project-gomoku/print_board.py
@@ -1,4 +1,5 @@
 def print_board(board, board_size):
+    # top side index print
     print("  ", end=" ")
     for i in range(board_size):
         if i < 9:
@@ -6,6 +7,8 @@ def print_board(board, board_size):
         else:
             print(i + 1, end=" ")
     print()
+
+    # both side index and inner value(".", "*", "X", "O") print
     for i in range(board_size):
         if i < 9:
             print(i + 1, end="  ")
@@ -13,4 +16,17 @@ def print_board(board, board_size):
             print(i + 1, end=" ")
         for j in range(board_size):
             print(board[i][j], end="  ")
+        if i < 9:
+            print(i + 1, end="  ")
+        else:
+            print(i + 1, end=" ")
         print()
+
+    # bottom side index print
+    print("  ", end=" ")
+    for i in range(board_size):
+        if i < 9:
+            print(i + 1, end="  ")
+        else:
+            print(i + 1, end=" ")
+    print()
